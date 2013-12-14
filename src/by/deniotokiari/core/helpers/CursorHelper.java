1a87884512a7fe26286d0cafb4053fc1bd3e9985
deniotokiari <deniotokiari@gmail.com>
2013-12-14 11:07:06 +0300

    new cursor helper methods

--- src/by/deniotokiari/core/helpers/CursorHelper.java
+++ src/by/deniotokiari/core/helpers/CursorHelper.java
@@ -1,11 +1,26 @@
 package by.deniotokiari.core.helpers;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 
 public class CursorHelper {
- 
-	public static String get(Cursor cursor, String key) {
-		return cursor.getString(cursor.getColumnIndex(key));
-	}
-	
+
+    public static String get(Cursor cursor, String key) {
+        return cursor.getString(cursor.getColumnIndex(key));
+    }
+
+    public static boolean isEmptyAndClosed(Cursor cursor) {
+        return (cursor.getCount() == 0) && (cursor.isClosed());
+    }
+
+    public static ContentValues getContentValues(Cursor cursor) {
+        ContentValues values = new ContentValues();
+
+        for (String name : cursor.getColumnNames()) {
+            values.put(name, get(cursor, name));
+        }
+
+        return values.keySet().size() == 0 ? null : values;
+    }
+
 }
